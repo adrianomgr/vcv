e86f4e8562aa06fb5f0c680c64f954ebc796246a
Adriano Gomes <adrianomgomesr@hotmail.com>
2018-11-30 13:04:57 -0300

    fix bug confirmação

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -209,13 +209,13 @@ class _OcrScreenState extends State<OcrScreen> {
                   return new SimpleDialog(
                     children: <Widget>[Text('Error: ${snapshot.error}')],
                   );
-                else if (snapshot.hasData && snapshot.data.status == 0)
+                else if (snapshot.hasData && snapshot.data.statusCode == 0)
                   return new SimpleDialog(
                     children: <Widget>[
                       _getCar(context, snapshot.data)
                     ],
                   );
-                else if (snapshot.hasData && snapshot.data.status != 0)
+                else if (snapshot.hasData && snapshot.data.statusCode != 0)
                   return new SimpleDialog(
                     children: <Widget>[
                       _getCar(context, snapshot.data),
@@ -310,7 +310,7 @@ class CarDatabase {
       .getDocuments();
       
     var all_results = await fireData.documents;
-    if (all_results.documents.isNotEmpty) {
+    if (all_results.isNotEmpty) {
       var result = all_results.first;
       if (result != null && result.exists) {
         print(result.data['plate']);
